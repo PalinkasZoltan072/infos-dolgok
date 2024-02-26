0a97db4fb8ec8f4d1519d1ecb7a3ec0c1d22feae
PALINKAS.ZOLTAN <palinkas.zoltan@ckik.hu>
2024-02-26 19:05:50 +0100

    listaban a lista

---
+++ 2024_02_23_ListabanLista/2024_02_23_ListabanLista/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_02_23_ListabanLista
+{
+    class Program
+    {
+        static List<List<int>> listak = new List<List<int>>();
+        static void Main(string[] args)
+        {
+            ListakFeltoltese();
+            ListakKiiratasa();
+            legkis();
+
+            // FVek
+            // Adja meg hányadik lista a leghosszabb!
+            // Adja meg azt a listát, amelyik tartalmaz 13-al osztható számot! Ha nincs írja ki, hogy nincs!
+            // Adja meg a legkisebb elem indexét! (sorát és elemszámát)
+
+            Console.ReadLine();
+        }
+
+
+        static void eldontes() {
+            
+            int sor = 0;
+            for (int i = 0; i < listak.Count; i++)
+            {
+                for (int j = 0; j < listak[i].Count; j++)
+                {
+                    if (listak[i][j] % 13 == 0)
+                    {
+                       
+                        sor = i;
+                    }
+                }
+                
+            }
+
+            if (sor > 0)
+            {
+                Console.WriteLine("van benne ebben a listaban:  " + sor);
+            }
+            else
+            {
+                Console.WriteLine("nincs");
+            }
+
+
+        }
+
+
+
+        static int leghosszabb()
+        {
+           
+            int hossz = -1;
+
+            for (int i = 0; i < listak.Count; i++)
+            {
+                if (listak[i].Count > hossz)
+                {
+                   
+                   hossz = i;
+                }
+            }
+
+            return hossz;
+        }
+
+        static int[] legkis()
+        {
+            int[] tomb = new int[5];
+            int legkisebb = listak[0][0];
+            int sor = 0;
+            int elemszam = 0;
+            for (int i = 0; i < listak.Count; i++)
+            {
+                for (int j = 0; j < listak[i].Count; j++)
+                {
+                    if (legkisebb > listak[i][j])
+                    {
+                        tomb[0] = i;
+                        tomb[1] = j;
+                        sor = i;
+                        elemszam = listak[i].Count;
+                    }
+                }
+            }
+
+            return tomb;
+           
+        }
+
+        static void ListakKiiratasa()
+        {
+            for (int i = 0; i < listak.Count; i++)
+            {
+                for (int j = 0; j < listak[i].Count; j++)
+                {
+                    Console.Write(listak[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ListakFeltoltese()
+        {
+            Random r = new Random();
+            int sor = 6;
+            //int[] elemszam = new int[] { 4, 7, 1, 3, 12, 4 };
+            int elemszam = r.Next(5,12);
+            for (int i = 0; i < sor; i++)
+            {
+                List<int> segedLista = new List<int>();
+                segedLista.Clear();
+                for (int j = 0; j < elemszam; j++)
+                {
+                    segedLista.Add(r.Next(10,100));
+                }
+                listak.Add(segedLista);
+            }
+
+        }
+    }
+}
